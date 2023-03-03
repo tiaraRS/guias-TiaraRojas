80ab1eb2f0ce4702b237f19a2337f25856bb6240
Tiara Rojas <tiara.rojas@niceincontact.com>
2023-03-02 20:25:03 -0400

    guia4 completa: Action Bar

--- app/src/main/java/com/ucb/guias/BackgroundActivity.kt
+++ app/src/main/java/com/ucb/guias/BackgroundActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class BackgroundActivity : AppCompatActivity() {
@@ -13,10 +14,16 @@ class BackgroundActivity : AppCompatActivity() {
         get()=findViewById(R.id.btnRed)
     private val layoutPrincipal: ConstraintLayout
         get()=findViewById(R.id.layoutPrincipal)
+    private val toolBar:Toolbar
+        get()=findViewById(R.id.toolbar)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_background)
+
+        supportActionBar.let{
+            setSupportActionBar(toolBar)
+        }
 
         btnRed.setOnClickListener{
             layoutPrincipal.setBackgroundColor(Color.RED)
